3a4454f3e408d85ac26f8005f9faa0b3c55d7af2
David Alves Dutra <dadutra@hotmail.com>
2025-01-08 20:40:42 -0300

    Adicionadas camadas Service e Repository. Department e Employee.

---
+++ src/main/java/com/devsuperior/demo/DepartmentController.java
@@ -0,0 +1,11 @@
+package com.devsuperior.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/departments")
+public class DepartmentController {
+
+
+}
